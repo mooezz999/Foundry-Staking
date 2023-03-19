0578e9f211a1d822c20748b13a2efede17c73d25
mooezz999 <moustafaezz999@gmail.com>
2023-03-19 19:36:08 +0200

    finished

---
+++ test/StakeContract.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "../src/StakeContract.sol";
+import "./mocks/MockERC20.sol";
+
+
+contract StakeContractTest is Test {
+    StakeContract public stakeContract;
+    MockERC20 public mockToken;
+    function setUp() public {
+        stakeContract = new StakeContract();
+        mockToken = new MockERC20();
+    }
+
+    function testExample(uint256 amount) public {
+        // uint256 amount = 10e18;
+        mockToken.approve(address(stakeContract), amount);
+        bool stakePassed = stakeContract.stake(amount, address(mockToken));
+        assertTrue(stakePassed);
+    }
+
+}
---
+++ test/mocks/MockERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20 ("Mock ERC20", "MERC") {
+        //This is the maximum value of uint256
+
+        _mint(msg.sender, 115792089237316195423570985008687907853269984665640564039457584007913129639935);
+    }
+
+}
